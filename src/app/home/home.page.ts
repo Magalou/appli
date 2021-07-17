985c9b5b7f443909ed58eb2c37da60027a814b6c
Magalou <magali.serre84@aol.fr>
2021-07-17 17:29:13 +0200

    premier

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  MovieApiUrl = '';
+  movieData = {
+    username:'',
+  }
+
+
+  constructor(public http:HttpClient) {
+    this.readAPI('https://jsonplaceholder.typicode.com/users')
+    .subscribe((data) => {
+      console.log(data)
+      this.movieData.username= data['username'];
+    });
+  }
+  readAPI(URL:string){
+    return this.http.get(URL);
+  }
 
 }
