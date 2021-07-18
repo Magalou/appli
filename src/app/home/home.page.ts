bf160d19d3d25cf273eeb3e3f9daa3d67fac4354
Magalou <magali.serre84@aol.fr>
2021-07-18 11:39:50 +0200

    preparation camera

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,19 +9,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
 
+  base64: string="data:image/png;base64";
   MovieApiUrl = '';
   movieData = {
     username:'',
   }
-
-
+  
   constructor(public http:HttpClient) {
     this.readAPI('https://jsonplaceholder.typicode.com/users')
     .subscribe((data) => {
       console.log(data)
       this.movieData.username= data['username'];
+        })
+      }
     });
   }
+
   readAPI(URL:string){
     return this.http.get(URL);
   }
